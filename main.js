2eccb0cd6bd3137d6b93a4b184a9ce54c88850b0
Sun Ruiheng <sunruiheng2001@gmail.com>
2021-09-27 02:46:49 +0800

    good start
    got the main functionality up and running, if only for the tables to show up. at least i can be confident the API works. a lot more to be done, more functionalities, and CSS-ing it into a pretty site.

--- main.js
+++ main.js
@@ -1,55 +1,50 @@
-const token = '7aa496fb7e9b4a2da942794924e18031';
-const baseURL = 'http://api.football-data.org/v2/';
+// const token = '7aa496fb7e9b4a2da942794924e18031';
+// const baseURL = 'http://api.football-data.org/v2/competitions/';
 
-const fetchResponse = async (url) => {
-    try {
-        const response = await fetch(url, {
-            headers: {
-                'X-Auth-Token': token
-            }
-        });
-        if (response.ok) {
-            const jsonResponse = await response.json();
-            return jsonResponse;
-        } 
-    } catch(e) {
-        console.log(e);
-    }
-};
+// const fetchResponse = async (url) => {
+//     try {
+//         const response = await fetch(url, {
+//             headers: {
+//                 'X-Auth-Token': token
+//             }
+//         });
+//         if (response.ok) {
+//             const jsonResponse = await response.json();
+//             return jsonResponse;
+//         } 
+//     } catch(e) {
+//         console.log(e);
+//     }
+// };
 
-const getStandings = async (league) => {
-    let urlToFetch = baseURL + 'competitions/' + league;
-    let jsonResponse = fetchResponse(urlToFetch);
-    console.log(jsonResponse);
-    console.log('BOOOOOOOOOOOOOOOO')
+// async function getStandings (league) {
+//     urlToFetch = baseURL + 'competitions/' + league;
+//     let jsonResponse = fetchResponse(urlToFetch);
+// }
+
+
+
+
+const href = './standings.html?league=';
+const goToStandings = (league) => {
+    location.href = href + league;
+}
+// ONCLICKS
+window.onload = function () {
+    const EPL = document.querySelector('#epl');
+    EPL.addEventListener('click', function() {
+        goToStandings('2021');
+    });
+    
+    const bundesliga = document.getElementById('bundesliga');
+    bundesliga.addEventListener('click', function () {
+        goToStandings('2002');
+    });
+
+    const laliga = document.getElementById('laliga');
+    laliga.addEventListener('click', function () {
+        goToStandings('2014');
+    });
 
 }
-
-
-
-// ONCLICKS
-if (document.querySelector('#epl')) {
-    const EPL = document.getElementById('epl');
-    EPL.addEventListener('click', function(event) {
-        
-        location.replace('standings.html');
-        getStandings('2021');
-        event.preventDefault();
-    }, false);
-    };
-if (document.querySelector('#bundesliga')) {
-    const bundesliga = document.getElementById('bundesliga');
-    bundesliga.addEventListener('click', function(event) {
-        getStandings('2088');
-        location.replace('standings.html');
-        event.preventDefault();
-    }, false);
-    };
-if (document.querySelector('#laliga')) {
-    const laliga = document.getElementById('laliga');
-    laliga.addEventListener('click', function(event) {
-        getStandings('2224');
-        location.replace('standings.html');
-        event.preventDefault();
-    }, false);
-    };
+    
--- standings.js
+++ standings.js
@@ -1,6 +1,6 @@
 
 const token = '7aa496fb7e9b4a2da942794924e18031';
-const baseURL = 'http://api.football-data.org/v2/';
+const baseURL = 'http://api.football-data.org/v2/competitions/';
 var teamCount = 0;
 
 const fetchResponse = async (url) => {
@@ -18,15 +18,78 @@ const fetchResponse = async (url) => {
         console.log(e);
     }
 };
-
+console.log(fetchResponse(baseURL))
 const getStandings = async (league) => {
-    let urlToFetch = baseURL + 'competitions/' + league;
-    let jsonResponse = fetchResponse(urlToFetch);
-    console.log(jsonResponse);
+    let urlToFetch = baseURL + league + '/standings';
+    let jsonResponse = await fetchResponse(urlToFetch);
+    console.log(jsonResponse); //delete later
+    let standings = jsonResponse.standings[0].table;
+    console.log(standings); //delete later
+    let tbody = document.getElementById('standings-body');
+    let tlength = standings.length;
+    for (let i = 0; i < tlength; i++) {
+        let row = document.createElement("tr");
+        tbody.appendChild(row);
+        // idk if i have to declare row again? by its id? but it has no id?
+        let td1 = document.createElement('td');
+        let data = document.createTextNode(i+1);
+        td1.appendChild(data);
+        row.appendChild(td1);
+        
+        let td2 = document.createElement('td');
+        let data2 = document.createTextNode(standings[i].team.name);
+        td2.appendChild(data2);
+        row.appendChild(td2);
+        
+        let td3 = document.createElement('td');
+        let data3 = document.createTextNode(standings[i].playedGames);
+        td3.appendChild(data3);
+        row.appendChild(td3);
+        
+        let td4 = document.createElement('td');
+        let data4 = document.createTextNode(standings[i].won);
+        td4.appendChild(data4);
+        row.appendChild(td4);
+        
+        let td5 = document.createElement('td');
+        let data5 = document.createTextNode(standings[i].draw);
+        td5.appendChild(data5);
+        row.appendChild(td5);
+        
+        let td6 = document.createElement('td');
+        let data6 = document.createTextNode(standings[i].lost);
+        td6.appendChild(data6);
+        row.appendChild(td6);
+        
+        let td7 = document.createElement('td');
+        let data7 = document.createTextNode(standings[i].points);
+        td7.appendChild(data7);
+        row.appendChild(td7);
+        
+        let td8 = document.createElement('td');
+        let data8 = document.createTextNode(standings[i].goalsFor);
+        td8.appendChild(data8);
+        row.appendChild(td8);
+        
+        let td9 = document.createElement('td');
+        let data9 = document.createTextNode(standings[i].goalsAgainst);
+        td9.appendChild(data9);
+        row.appendChild(td9);
+        
+        let td10 = document.createElement('td');
+        let data10 = document.createTextNode(standings[i].goalDifference);
+        td10.appendChild(data10);
+        row.appendChild(td10);
 
+    }
+    
 }
 
-
+const queryString = window.location.search;
+const urlParams = new URLSearchParams(queryString);
+const league = urlParams.get('league');
+getStandings(league);
+// getStandings(league);
 
 // ASSIGNING VAR TO DOMS
 
