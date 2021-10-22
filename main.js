484d80478190eebcb0f4bd3a0e6d39ec1508d2bc
Sun Ruiheng <sunruiheng2001@gmail.com>
2021-10-22 22:22:46 +0800

    testing react stuff...

--- main.js
+++ main.js
@@ -49,18 +49,3 @@ window.onload = function () {
 }
     
 
-
-import React from 'react';
-import ReactDOM from 'react-dom';
-
-class TestComponentClass extends React.Component {
-    render() {
-        return (
-            <h1>AYOOOOO lmaoooo</h1>
-        );
-    }
-}
-ReactDOM.render(
-    <TestComponentClass />,
-    document.getElementById('epl')
-)
