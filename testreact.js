4154611e77322cf3cf02202c24865b4d9ddf2b0a
Sun Ruiheng <sunruiheng2001@gmail.com>
2021-10-22 22:21:47 +0800

    trying out react

---
+++ testreact.js
@@ -0,0 +1,15 @@
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class TestComponentClass extends React.Component {
+    render() {
+        return (
+            <h1>AYOOOOO lmaoooo</h1>
+        );
+    }
+}
+ReactDOM.render(
+    <TestComponentClass />,
+    document.getElementById('epl')
+)
